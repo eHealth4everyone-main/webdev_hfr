24d3d1f5f76a5e14d1e63406111ed3f05fbc537b
DESKTOP-CP0DE0J\Beatus.Kibiti <beatuskibiti@gmail.com>
2018-09-17 11:34:31 +0100

    public master formatted

--- resources/views/layouts/public_master.blade.php
+++ resources/views/layouts/public_master.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>HFR | Nigeria</title>
+  <title>Nigeria Health Facility Registry</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
   <link rel="stylesheet" href="{{ asset("dist/css/font-awesome/css/font-awesome.min.css")}}" >
@@ -13,67 +13,73 @@
   <link rel="stylesheet" href="{{ asset("dist/css/bootstrap-datepicker.min.css")}}">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-  <link rel="stylesheet" href="{{ asset("dist/css/skins/skin-green.min.css")}}">
+  {{-- <link rel="stylesheet" href="{{ asset("dist/css/skins/skin-green.min.css")}}"> --}}
 	<link rel="stylesheet" href="{{asset("styles/style-resp.css")}}" type="text/css" media="all" />
 
   @yield("kibiti_css")
 </head>
-<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
-<body >
 
-        
-    <div class="container"  id="container">
-        <div class="header">
-          <div class="coatofarmsdiv">
-            <img title="Ministry of Health - Federal Republic Of Nigeria" class="" src="img/logo.png" width="90" height="76" />
-          </div>
-          <div class="headertext">
-            NIGERIA HEALTH FACILITY REGISTRY
-            <div class="subheadertext">... a project of the Federal Ministry of Health supported by the United States Agency for International Development</div>
-          </div>
-          <div class="usaiddiv">
-            <img title="United States Agency International Development" class="" src="img/usaid.png"/>
-          </div>
+<body >
+    <nav class="navbar navbar-default navbar-static-top">
+
+      <div class="containerx"  id="containerx">
+          <div class="headerx">
+            <div class="coatofarmsdiv">
+              <img title="Ministry of Health - Federal Republic Of Nigeria" class="" src="img/logo.png" width="90" height="76" />
+            </div>
+            <div class="headertextx">
+              NIGERIA HEALTH FACILITY REGISTRY
+              <div class="subheadertext">... a project of the Federal Ministry of Health supported by the United States Agency for International Development</div>
+            </div>
+            <div class="usaiddiv">
+              <img title="United States Agency International Development" class="" src="img/usaid.png"/>
+            </div>
+          </div>       
         </div>
-        <div class="navigation">
-            <nav role="navigation" class="navbar navbar-default">
-              <div id="navbarCollapse" class="collapse navbar-collapse">
-                  <ul class="nav navbar-nav navbar-default">
-                      <li><a href="{{route('home')}}">Home</a></li>
-                      <li><a href="{{route('about')}}">About</a></li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Facility List <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="{{route('listhosp')}}">Hospitals</a></li>
-                          <li><a href="{{route('listlab')}}">Laboratories</a></li>
-                          <li><a href="{{route('listpharmacy')}}">Pharmaceuticals</a></li>
-                          <li><a href="{{route('listradiology')}}">Radiology and Imaging</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="#">Statistics</a></li>
-                      <li><a href="#">Facility Search</a></li>
-                      <li><a href="{{route('public_resources')}}">Resources</a></li>
-                      <li><a href="#">Contact us</a></li>
-                      <li><a href="{{route('admin_home')}}">Login</a></li>
+           	
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              {{-- <a class="navbar-brand" href="#">HFR</a> --}}
+            </div>
         
-                    </ul>
-                
-              </div>
-            </nav>
-          </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar1">
+                <ul class="nav navbar-nav navbar-default">
+                    <li><a href="{{route('home')}}">Home</a></li>
+                    <li><a href="{{route('about')}}">About</a></li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Facilities List <span class="caret"></span></a>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{route('listhosp')}}">Hospitals</a></li>
+                        <li><a href="{{route('listlab')}}">Laboratories</a></li>
+                        <li><a href="{{route('listpharmacy')}}">Pharmaceuticals</a></li>
+                        <li><a href="{{route('listradiology')}}">Radiology and Imaging</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#">Statistics</a></li>
+                    <li><a href="#">Data Downloads</a></li>
+                    <li><a href="{{route('public_resources')}}">Resources</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="{{route('admin_home')}}">Administrator</a></li>
       
-        {{-- <div class="collapse navbar-collapse pull-right" id="navbar-collapse">
-          
-         
-        </div>	 --}}
-      </div>
-     	
+                  </ul>
+           
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
 
  
  
 
   <!-- Full Width Column -->
-  <div class="box">
+  
     <div class="container">
       <!-- Content Header (Page header) -->
       <section class="content-header">
@@ -89,9 +95,9 @@
       <!-- /.content -->
     </div>
     <!-- /.container -->
-  </div>
+  
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
+  <div class="well well-lg">
     <div class="container">
       <div class="pull-right hidden-xs">
         <b>Version</b> 2.0.0
@@ -100,7 +106,7 @@
       reserved.
     </div>
     <!-- /.container -->
-  </footer>
+  </div>
 
 <!-- ./wrapper -->
 
