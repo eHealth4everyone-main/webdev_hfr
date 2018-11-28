4889385a8f171e89efa345bbd71548465eedb399
DESKTOP-CP0DE0J\Beatus.Kibiti <beatuskibiti@gmail.com>
2018-11-28 15:15:09 +0100

    modal to master

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -49,24 +49,6 @@
 </div>
 
 
-<!-- Goolge Map Modal -->
-<div class="modal fade" id="googleMapModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"  >
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Facilities in LGA</h4>
-            </div>
-            <div class="modal-body">
-                <div id="googleMap" style="height: 500px; min-width: 500px; max-width: 800px; margin: 0 auto"></div>
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 @endsection 
