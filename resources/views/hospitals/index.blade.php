4245a06bbbf839500d2fec320348efcd622a52c5
DESKTOP-CP0DE0J\Beatus.Kibiti <beatuskibiti@gmail.com>
2019-01-14 10:50:55 +0100

    lga permsiion update

--- resources/views/hospitals/index.blade.php
+++ resources/views/hospitals/index.blade.php
@@ -112,7 +112,7 @@ Hospitals and Clinics
                                 View
                             </button>
                         </a> 
-                        @if (($fac->lga_id == Auth::user()->lga_id) OR ($fac->lga_id == 1))                    
+                        @if (($fac->lga_id == Auth::user()->lga_id) OR (Auth::user()->lga_id == 1))                    
                             @if(auth()->user()->hasPermissionTo(3))
                             <a href="{{route('hospitals.edit',$fac->id)}}">
                                 <button class="btn btn-warning btn-sm"  type="button" >Edit</button>
