84d9446a7d5a78441559e41f971474d60ec268fc
DESKTOP-CP0DE0J\Beatus.Kibiti <beatuskibiti@gmail.com>
2019-01-27 17:40:07 +0100

    population index offline download

--- resources/views/public/statistic_population_index.blade.php
+++ resources/views/public/statistic_population_index.blade.php
@@ -47,9 +47,10 @@
 @endsection 
 
 @push('custom_scripts')
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/data.js"></script>
+<script src="{{ asset("hcharts/chart/highcharts.js")}}"></script>
+<script src="{{ asset("hcharts/chart/exporting.js")}}"></script>
+<script src="{{ asset("hcharts/chart/data.js")}}"></script>
+<script src="{{ asset("hcharts/chart/offline-exporting.js")}}"></script>   
 
 
 <script> 
