0acd17a9fe3c78dd02fa588de2cce1c6a1d0ba36
DESKTOP-CP0DE0J\Beatus.Kibiti <beatuskibiti@gmail.com>
2019-05-10 07:57:20 +0100

    minor updates

--- resources/views/public/facilities_updates.blade.php
+++ resources/views/public/facilities_updates.blade.php
@@ -16,7 +16,7 @@
             @csrf
 
             <div class="form-group">
-                 <label class="col-sm-3  control-label" >Facility Updates Report</label>
+                 {{-- <label class="col-sm-3  control-label" >Facility Updates Report</label> --}}
                 <div class="col-sm-10">
                     <select class="form-control select2" id="report" name="report" required>
                         <option value="">--Select Report--</option>
