d1479cc5d92df293cee09a28331cc07f9e0d6032
DESKTOP-CP0DE0J\Beatus.Kibiti <beatuskibiti@gmail.com>
2019-06-14 13:17:09 +0100

    bug

--- routes/api.php
+++ routes/api.php
@@ -14,24 +14,5 @@ use Illuminate\Http\Request;
 */
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
-//         // Route::get('/hfr','apiController@index');
-//         // Route::post('post','apiController@store');
-   
+//         return $request->user();
 // });
-
-// Route::post('post','apiController@store');
-// // Route::get('/hfr','apiController@index');
-
-// Route::get('/hfr','apiController@collection');
-
-// Route::get('/user', function (Request $request) {
-//     $users= \App\User::all();
-//         return response()->json([
-//             "code"=>200,
-//             "status"=>"success",
-//             "message"=>"transaction was successful",
-//             "DeveloperMessage"=>"the list of users was ftche3d sucessfully",
-//             "data"=>["users"=>$users]
-//         ],200); 
-// });
-
