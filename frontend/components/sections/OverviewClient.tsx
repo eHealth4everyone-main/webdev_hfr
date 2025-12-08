70ed2b1a668e15bbc65a11640b1799e7bbcea9eb
e4ewebdev <webdev@e4email.net>
2025-12-08 20:57:10 +0100

    added embed map

--- frontend/components/sections/OverviewClient.tsx
+++ frontend/components/sections/OverviewClient.tsx
@@ -6,66 +6,73 @@ import OverviewMap from "@/components/sections/overview/OverviewMap";
 import Table from "@/components/sections/overview/Table";
 import SelectComponent from "@/components/ui/SelectComponent";
 import { GreenButton, Heading, WhiteButton } from "@/components/ui/Typography";
+import { useEffect } from "react";
 
-const OverviewClient = () => {
-  const [view, setView] = useState<"map" | "chart" | "table">("map");
+export default function OverviewClient() {
+  useEffect(() => {
+    const divElement = document.getElementById("viz1765138662074");
+    if (!divElement) return;
+
+    const vizElement = divElement.getElementsByTagName("object")[0];
+
+    // Responsive sizing
+    if (divElement.offsetWidth > 800) {
+      vizElement.style.width = "1350px";
+      vizElement.style.height = "677px";
+    } else if (divElement.offsetWidth > 500) {
+      vizElement.style.width = "1350px";
+      vizElement.style.height = "677px";
+    } else {
+      vizElement.style.width = "100%";
+      vizElement.style.height = "1727px";
+    }
+
+    // Load Tableau JS API dynamically
+    const scriptElement = document.createElement("script");
+    scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+  }, []);
 
   return (
-    <div className="pt-32 px-8">
-      <div className="flex flex-wrap justify-between items-start gap-4 lg:gap-0">
-        <div>
-          <Heading>Overview</Heading>
-        </div>
+    <div className="w-full mt-20 px-4">
+      {/* Tableau Embed */}
+      <div
+        className="tableauPlaceholder"
+        id="viz1765138662074"
+        style={{ position: "relative" }}
+      >
+        <noscript>
+          <a href="#">
+            <img
+              alt="Nigeria’s Healthcare Facility Ownership Landscape"
+              src="https://public.tableau.com/static/images/He/HealthFacilityRegistryDashboardNew/NigeriasHealthcareFacilityOwnershipDashboar/1_rss.png"
+              style={{ border: "none" }}
+            />
+          </a>
+        </noscript>
 
-        <div className="flex flex-wrap lg:flex-nowrap gap-2">
-          <SelectComponent />
-          <SelectComponent />
-          <GreenButton className="w-[180px]">Search</GreenButton>
-        </div>
-
-        <div className="flex flex-wrap gap-2">
-          <WhiteButton
-            className={
-              view === "map"
-                ? "bg-[#5BBA62] text-white border-0"
-                : "transparent"
-            }
-            onClick={() => setView("map")}
-          >
-            Map
-          </WhiteButton>
-
-          <WhiteButton
-            className={
-              view === "chart"
-                ? "bg-[#5BBA62] text-white border-0"
-                : "transparent"
-            }
-            onClick={() => setView("chart")}
-          >
-            Chart
-          </WhiteButton>
-
-          <WhiteButton
-            className={
-              view === "table"
-                ? "bg-[#5BBA62] text-white border-0"
-                : "transparent"
-            }
-            onClick={() => setView("table")}
-          >
-            Table
-          </WhiteButton>
-        </div>
-      </div>
-
-      <div className="w-full max-w-full min-h-[400px] mt-8">
-        {view === "map" && <OverviewMap />}
-        {view === "chart" && <Chart />}
-        {view === "table" && <Table />}
+        <object className="tableauViz" style={{ display: "none" }}>
+          <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
+          <param name="embed_code_version" value="3" />
+          <param name="site_root" value="" />
+          <param
+            name="name"
+            value="HealthFacilityRegistryDashboardNew/NigeriasHealthcareFacilityOwnershipDashboar"
+          />
+          <param name="tabs" value="no" />
+          <param name="toolbar" value="yes" />
+          <param
+            name="static_image"
+            value="https://public.tableau.com/static/images/He/HealthFacilityRegistryDashboardNew/NigeriasHealthcareFacilityOwnershipDashboar/1.png"
+          />
+          <param name="animate_transition" value="yes" />
+          <param name="display_static_image" value="yes" />
+          <param name="display_spinner" value="yes" />
+          <param name="display_overlay" value="yes" />
+          <param name="display_count" value="yes" />
+          <param name="language" value="en-US" />
+        </object>
       </div>
     </div>
   );
-};
-
-export default OverviewClient;
+}
